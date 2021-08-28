13bbbe196541158070e494edc97247c5bb8ac6e6
realOmkarBirje <89650949+realOmkarBirje@users.noreply.github.com>
2021-08-28 18:57:04 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'e8228741ff6549aba49600e91fd5b46a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'get Indie Latte',
+  domain: 'get.indielatte.com',
+  author: 'indie latte',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'Products for indie hackers',
+  socialImageTitle: 'indielatte',
+  socialImageSubtitle: 'Hello IndieHacker! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'indielattehq',
+  github: 'realomkarbirje',
+  linkedin: 'realomkarbirje',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
